1aad3e255a29f496890f9b680d76d6fbf1fba4e8
coiiector <coIIector@users.noreply.github.com>
2016-03-26 23:33:38 +0100

    firefox fix

--- js/search.js
+++ js/search.js
@@ -1,1 +1,4 @@
-var h=decodeURIComponent(window.location.hash),i=h.indexOf(" "),s;window.location=i>1&&(s=localStorage.getItem("search:"+h.slice(1,i)))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(i+1)))||(s=localStorage.getItem("search:DEFAULT"))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(1)))||"searchSetup.html"+h;
+var h=window.location.hash,i,s;
+if(h.match(/^#[a-zA-Z0-9]+\+/))h=h.replace(/\+/g,' ');
+h=decodeURIComponent(h);
+i=h.indexOf(" ");window.location=i>1&&(s=localStorage.getItem("search:"+h.slice(1,i)))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(i+1)))||(s=localStorage.getItem("search:DEFAULT"))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(1)))||"searchSetup.html"+h;
