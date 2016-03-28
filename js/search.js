94537ce5dccab5d9dfc8582c42f2382c2c393da7
coiiector <coIIector@users.noreply.github.com>
2016-03-28 22:08:48 +0200

    external_url as parameter to search.js

--- js/search.js
+++ js/search.js
@@ -1,4 +1,14 @@
-var h=window.location.hash,i,s;
-if(h.match(/^#[a-zA-Z0-9]+\+/))h=h.replace(/\+/g,' ');
-h=decodeURIComponent(h);
-i=h.indexOf(" ");window.location=i>1&&(s=localStorage.getItem("search:"+h.slice(1,i)))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(i+1)))||(s=localStorage.getItem("search:DEFAULT"))&&s.replace(/\{ARG}/g,encodeURIComponent(h.slice(1)))||"searchSetup.html"+h;
+var h = window.location.hash, i, s, l, k;
+if (h.match(/^#[a-zA-Z0-9]+\+/))h = h.replace(/\+/g, ' ');
+h = decodeURIComponent(h);
+i = h.indexOf(" ");
+l = i > 1 && (s = localStorage.getItem("search:" + h.slice(1, i))) && s.replace(/\{ARG}/g, encodeURIComponent(h.slice(i + 1))) || (s = localStorage.getItem("search:DEFAULT")) && s.replace(/\{ARG}/g, encodeURIComponent(h.slice(1)));
+if (l || localStorage.getItem('search:EXTERNAL') || !(k = window.location.search.slice(1)))window.location = l || "searchSetup.html" + h; else {
+    window.loadFromJson = function (data) {
+        $.each(data, function (key, value) {
+            localStorage.setItem('search:' + key, value);
+            location.reload();
+        })
+    };
+    $.getScript(k);
+}
